d689207b18786c45257feea56f2a78caf7d5ad7b
billzxy <billzxy1214@hotmail.com>
2026-01-03 19:31:57 +0800

    Update proxy.ts

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -3,7 +3,7 @@ export const config = {
   // Available languages and regions for Google AI Studio and Gemini API
   // https://ai.google.dev/available_regions
   // https://vercel.com/docs/concepts/edge-network/regions
-  regions: ['hkg1', 'cle1', 'iad1', 'pdx1', 'sfo1', 'sin1', 'syd1', 'hnd1', 'kix1'],
+  regions: ['cle1', 'iad1', 'pdx1', 'sfo1', 'sin1', 'syd1', 'hnd1', 'kix1'],
 }
 
 export { default } from '@/index'
